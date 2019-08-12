46ee05f2bb463a8498368ba48ea1b8e574eadfde
KrivulyaElena <krivulyaelena@gmail.com>
2019-08-12 04:01:51 +0300

    1st

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(function(){
+    $(".datepicker").datepicker({
+        dateFormat: 'dd.mm.yy', 
+        showButtonPanel: true,
+        // showOn: "button",
+        //  buttonImage: "images/calendar.png",
+        // buttonImageOnly: true,
+        // buttonText: "Select date",
+        
+    });
+  
+});
